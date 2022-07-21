2c53502c092fc470ab25426cd1e95ce0deb293ab
Benvinda Vedes <bafonso.vedes@gmail.com>
2022-07-21 14:07:15 +0100

    grid flex

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,27 @@
 const Flexboxone = () => {
-  return <div className="container">My Css and Flexbox.</div>;
+  const words = [
+    "Name",
+    "Número",
+    "Turma",
+    "Ano",
+    "Género",
+    "Benedita I. A. Vedes",
+    "3",
+    "B",
+    "Primeiro",
+    "Femenino",
+  ];
+  return (
+    <div className="flex grid grid-cols-5 gap-3 p-3 justify-center m-4 md:bg-yellow-200 lg:bg-blue-200 sm:bg-green-200 xl:bg-orange-200">
+      {words.map((word, idx) => (
+        <div
+          key={idx}
+          className="flex justify-center w-34 border border-indigo-600"
+        >
+          {word}
+        </div>
+      ))}
+    </div>
+  );
 };
 export default Flexboxone;
