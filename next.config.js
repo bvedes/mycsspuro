b1a722f24243c9ab89ae9268ae2df0900357e1cb
Benvinda Vedes <bafonso.vedes@gmail.com>
2022-08-27 15:13:32 +0100

    feat-certain-images-stylize

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: [
+      // são indereços das imagens. preciso ver quando elas sairem da minha pasta como fica.
       "blog.petiko.com.br",
       "i.pinimg.com",
       "cdn.los-animales.org",
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import { useData } from "../hooks/useData";
+import { ImPencil, ImBin } from "react-icons/im";
+
 import Image from "next/image";
 const Flexboxone = () => {
   const [data, setData] = useData();
@@ -6,39 +8,70 @@ const Flexboxone = () => {
   console.log("data.profiles: ", data.profiles);
 
   return (
-    <div className="table w-full">
-      <div className="table-header-group">
-        <div className="table-row">
-          {data?.personalData.map((pers, idx) => (
-            <div key={idx} className="table-cell text-left p-2">
-              {pers}
-            </div>
-          ))}
-        </div>
+    <>
+      <div className="bg-gray-200 p-4 h-screen">
+        <table className="border-collapse w-full">
+          <thead>
+            <tr>
+              {data?.personalData.map((pers, idx) => (
+                <th
+                  key={idx}
+                  className="font-bold uppercase bg-gray-200 text-gray-600 border border-gray-300 hidden lg:table-cell"
+                >
+                  {pers}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {data?.profiles.map(
+              (
+                { avatar, name, email, specialty, address, postalCode },
+                index
+              ) => (
+                <tr
+                  key={index}
+                  className="bg-white lg:hover:bg-gray-200 flex lg:table-row flex-row lg:flex-row flex-wrap lg:flex-no-wrap mb-4 lg:mb-0"
+                >
+                  <td className="w-full lg:w-auto p-3 text-gray-800 text-center border-b lg:table-cell relative">
+                    <div className="flex items-center">
+                      <Image
+                        className="rounded-full"
+                        src={avatar}
+                        alt="Picture"
+                        width={30}
+                        height={30}
+                      />
+                      <span className="ml-4 font-medium text-slate-900">
+                        {name}
+                      </span>
+                    </div>
+                  </td>
+                  <td className="w-full lg:w-auto p-3 text-gray-800 text-center border-b  lg:table-cell relative">
+                    <div className="flex items-center">{specialty}</div>
+                  </td>
+                  <td className="w-full lg:w-auto p-3 text-gray-800 text-center border-b  lg:table-cell relative">
+                    <div className="flex items-center">{email}</div>
+                  </td>
+                  <td className="w-full lg:w-auto p-3 text-gray-800 text-center border-b lg:table-cell relative">
+                    <div className="flex items-center">{address}</div>
+                  </td>
+                  <td className="w-full lg:w-auto p-3 text-gray-800 text-center border-b lg:table-cell relative">
+                    <div className="flex items-center">{postalCode}</div>
+                  </td>
+                  <td className="w-full lg:w-auto p-3 text-gray-800 text-center border-b lg:table-cell relative">
+                    <div className="flex items-center justify-between">
+                      <ImPencil />
+                      <ImBin />
+                    </div>
+                  </td>
+                </tr>
+              )
+            )}
+          </tbody>
+        </table>
       </div>
-      <div className="table-row-group">
-        {data?.profiles.map(
-          ({ avatar, name, email, specialty, address, postalCode }, index) => (
-            <div key={index} className="table-row">
-              <Image
-                className=""
-                src={avatar}
-                alt="Picture"
-                width={50}
-                height={50}
-              />
-
-              <div className="table-cell">{name}</div>
-              <div className="table-cell">{specialty}</div>
-              <div className="table-cell">{email}</div>
-
-              <div className="table-cell">{address}</div>
-              <div className="table-cell">{postalCode}</div>
-            </div>
-          )
-        )}
-      </div>
-    </div>
+    </>
   );
 };
 export default Flexboxone;
